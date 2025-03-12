db301cf61d71cee4e0e452ddb90fc2476d6432e1
Timur Denisenko <timur.denisenko.2887@gmail.com>
2025-03-13 00:07:26 +0200

    Attacking an enemy using a ranged attack class with animation but no trigger

---
+++ Assets/Scripts/NPC/NPCAnimationEvent.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.NPC
+{
+    public class NPCAnimationEvent : MonoBehaviour
+    {
+        public GameObject InteractionObject;
+        public GameObject InteractionObjectAnimation;
+        public float InteractionObjectSpeed;
+        private float initialZCoordinate;
+
+        void Update()
+        {
+            if (InteractionObject.activeSelf)
+                ChangeZCoordinate(InteractionObjectSpeed, true);
+            else
+                ChangeZCoordinate(initialZCoordinate);
+        }
+
+        public void StartSingleMagicAttack()
+        {
+            InteractionObject.SetActive(true);
+            InteractionObjectAnimation.SetActive(true);
+            initialZCoordinate = InteractionObject.transform.localPosition.z;
+        }
+        public void EndSingleMagicAttack()
+        {
+            InteractionObject.SetActive(false);
+            InteractionObjectAnimation.SetActive(false);
+        }
+
+        private void ChangeZCoordinate(float z, bool isAddition = false)
+        {
+            Vector3 pos = InteractionObject.transform.localPosition;
+            if (isAddition)
+                pos.z += z;
+            else
+                pos.z = z;
+            InteractionObject.transform.localPosition = pos;
+        }
+    }
+}
