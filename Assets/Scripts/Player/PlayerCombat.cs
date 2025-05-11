f45955fcd3880d7c3f55e71972075b21d0c19f01
Timur Denisenko <timur.denisenko.2887@gmail.com>
2025-05-11 07:31:25 +0300

    The player's combat logic has been slightly changed

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -24,10 +24,6 @@ public class PlayerCombat : MonoBehaviour
             else if (animator.GetCurrentAnimatorStateInfo(3).IsName("Second attack"))
             {
                 animator.SetBool("Hit2", false);
-            }
-            else if (animator.GetCurrentAnimatorStateInfo(3).IsName("Final attack"))
-            {
-                animator.SetBool("Hit3", false);
                 hitNumber = 0;
             }
         }
@@ -40,24 +36,17 @@ public class PlayerCombat : MonoBehaviour
     public void Attack()
     {
         lastClickedTime = Time.time;
-        hitNumber = Mathf.Clamp(++hitNumber, 0, 3);
+        hitNumber = Mathf.Clamp(++hitNumber, 0, 2);
         if (hitNumber == 1)
         {
             animator.SetBool("Hit1", true);
-            animator.SetBool("Hit3", false);
+            animator.SetBool("Hit2", false);
             return;
         }
         if (hitNumber == 2 && animator.GetCurrentAnimatorStateInfo(3).normalizedTime > 0.3f)
         {
             animator.SetBool("Hit1", false);
             animator.SetBool("Hit2", true);
-            return;
-        }
-        if (hitNumber == 3 && animator.GetCurrentAnimatorStateInfo(3).normalizedTime > 0.3f)
-        {
-            animator.SetBool("Hit2", false);
-            animator.SetBool("Hit3", true);
-            hitNumber = 0;
             return;
         }
     }
